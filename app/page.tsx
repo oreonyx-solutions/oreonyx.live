77a650eedd1be3426910103ae25f3320a5092526
Ayen Bonsoa <bonsoa.yen@gmail.com>
2023-05-21 18:00:04 +0000

    Feat: change new article banner text

--- app/page.tsx
+++ app/page.tsx
@@ -23,9 +23,7 @@ export default function Home() {
       <div className="bg-black py-2 text-white flex justify-center items-center relative">
         <span className="line-clamp-1">
           <a href="#" className="hover:underline">
-            <span className="lg:hidden">
-              Participez au Programme de Bêta Testing
-            </span>
+            <span className="lg:hidden">Nouvel article: Bêta Testing !</span>
             <span className="hidden lg:inline">
               Participez au Programme de Bêta Testing d&apos;Oreonyx :
               Contribuez à façonner la prochaine génération d&apos;outils
