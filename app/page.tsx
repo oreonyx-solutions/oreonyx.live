5442ffe447cc45912809e5da3c7d29215eca0fa4
Ayen Bonsoa <bonsoa.yen@gmail.com>
2023-05-23 20:14:21 +0000

    Fix:Update language configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,50 @@
 "use client";
 
 import { useState } from "react";
-import { useIntl } from "react-intl";
+import { IntlProvider, useIntl } from "react-intl";
 
-//components
 import Navbar from "@/components/ui/sections/Navbar";
 import Hero from "@/components/ui/sections/Hero";
 import Faq from "@/components/ui/sections/Faq";
 import CTA from "@/components/ui/sections/CTA";
 import Footer from "@/components/ui/sections/Footer";
 import Overview from "@/components/ui/sections/Overview";
+import enMessages from "@/locales/en.json";
+import frMessages from "@/locales/fr.json";
 
-// Rest of the imports and component code
+interface Messages {
+  [key: string]: { [key: string]: string };
+}
+
+const messages: Messages = {
+  en: enMessages,
+  fr: frMessages,
+};
+
 export default function Home() {
   const intl = useIntl();
-  const [language, setLanguage] = useState("fr"); // Initialize the language state with the default language
+  const [language, setLanguage] = useState("fr");
 
   const handleLanguageChange = (newLanguage: string) => {
     setLanguage(newLanguage);
   };
 
   return (
-    <div className="bg-primary">
-      <h1>{intl.formatMessage({ id: "home.title" })}</h1>
-      <Navbar />
-      <Hero />
-      <div className="md:px-6 text-gray-900">
-        <Overview />
-        <Faq />
-        <CTA />
-        <Footer language={language} onLanguageChange={setLanguage} />
+    <IntlProvider
+      locale={language}
+      messages={messages[language as keyof Messages]}
+    >
+      <div className="bg-primary">
+        <h1>{intl.formatMessage({ id: "home.title" })}</h1>
+        <Navbar />
+        <Hero />
+        <div className="md:px-6 text-gray-900">
+          <Overview />
+          <Faq />
+          <CTA />
+          <Footer language={language} onLanguageChange={setLanguage} />
+        </div>
       </div>
-    </div>
+    </IntlProvider>
   );
 }
