27b0493dcef4d8f4b0b2f3ea3b6ed1018976e32b
Ayen Bonsoa <bonsoa.yen@gmail.com>
2023-05-24 09:26:24 +0000

    Feat:set head metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,30 @@ export const metadata = {
     "Unified management, communication and networking platform for universities, businesses and individuals",
   description:
     "A revolutionary digital platform for universities, students and businesses, promoting access to higher education and employability of young people.",
+  og: {
+    title: "Oreonyx Technologies",
+    type: "website",
+    imageAlt: "Join Oreonyx Technologies",
+    url: "https://www.oreonyx.com",
+    description:
+      "A revolutionary digital platform for universities, students and businesses, promoting access to higher education and employability of young people.",
+    image:
+      "https://res.cloudinary.com/shogunmp7/image/upload/v1675845644/oreonyx_swzmsi.png",
+    locale: "en_US",
+    siteName: "Oreonyx",
+  },
+  twitter: {
+    card: "summary_large_image",
+    site: "@oreonyxHQ",
+    creator: "@oreonyxHQ",
+    url: "https://oreonyx.com",
+    imageAlt: "Join Oreonyx Technologies",
+    title: "Oreonyx Technologies",
+    description:
+      "A revolutionary digital platform for universities, students and businesses, promoting access to higher education and employability of young people.",
+    image:
+      "https://res.cloudinary.com/shogunmp7/image/upload/v1675845644/oreonyx_swzmsi.png",
+  },
 };
 
 export default function RootLayout({
@@ -22,42 +46,33 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000" />
-        <meta property="og:title" content="Oreonyx Solutions" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image:alt" content="Rejoignez Oreonyx Solutions" />
-        <meta property="og:url" content="https://www.oreonyx.com" />
+
+        {/* Open Graph */}
+        <meta property="og:title" content={metadata.og.title} />
+        <meta property="og:type" content={metadata.og.type} />
+        <meta property="og:image:alt" content={metadata.og.imageAlt} />
+        <meta property="og:url" content={metadata.og.url} />
+        <meta property="og:description" content={metadata.og.description} />
+        <meta property="og:image" content={metadata.og.image} />
+        <meta property="og:locale" content={metadata.og.locale} />
+        <meta property="og:site_name" content={metadata.og.siteName} />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content={metadata.twitter.card} />
+        <meta name="twitter:site" content={metadata.twitter.site} />
+        <meta name="twitter:creator" content={metadata.twitter.creator} />
+        <meta property="twitter:url" content={metadata.twitter.url} />
         <meta
-          property="og:description"
-          content="  A revolutionary digital platform for universities, students and
-          businesses, promoting access to higher education and employability of
-          young people."
+          property="twitter:image:alt"
+          content={metadata.twitter.imageAlt}
         />
-        <meta
-          property="og:image"
-          content="https://res.cloudinary.com/shogunmp7/image/upload/v1675845644/oreonyx_swzmsi.png"
-        />
-        <meta property="og:locale" content="fr_FR" />
-        <meta property="og:locale:alternate" content="fr_FR" />
-        <meta property="og:site_name" content="Oreonyx" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@oreonyxHQ" />
-        <meta name="twitter:creator" content="@oreonyxHQ" />
-        <meta property="twitter:url" content="https://oreonyx.com" />
-        <meta
-          property="twitter:image:alt "
-          content="Rejoignez Oreonyx Solutions"
-        />
-        <meta property="twitter:title" content="Oreonyx Solutions" />
+        <meta property="twitter:title" content={metadata.twitter.title} />
         <meta
           property="twitter:description"
-          content="Plateforme unifiée pour booster la productivité et les interactions 
-        au sein des universités via des outils complets et optimisées 
-        de gestion, de communication et de réseautage."
+          content={metadata.twitter.description}
         />
-        <meta
-          name="twitter:image"
-          content="https://res.cloudinary.com/shogunmp7/image/upload/v1675845644/oreonyx_swzmsi.png"
-        />
+        <meta name="twitter:image" content={metadata.twitter.image} />
+
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
